5c73cd492da26e7c434b24cd7f23f4588a44cfd1
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-10-28 18:25:25 +0300

    Добавьте файлы проекта.

---
+++ Lamda/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Lamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            int g = 0;
+            Console.WriteLine("Введите число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (a < 0)
+                ++s;
+            if (b < 0)
+                ++s;
+            if (c < 0)
+                ++s;
+            else
+                if (a > 0)
+                ++g;
+            if (b > 0)
+                ++g;
+            if (c > 0)
+                ++g;
+            Console.Write("Положительные числа:{0}", g);
+            Console.Write("Отрицательные: {0}", s);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
